bdd7bb39ba183aa6e830f51a60671a0939564626
BonifasiusPratama <imailanyar123@gmail.com>
2018-12-30 16:48:35 +0700

    commit1

---
+++ app/src/main/java/com/gmail/imailanyar123/beritaakakom_tama/DataBerita.java
@@ -0,0 +1,7 @@
+package com.gmail.imailanyar123.beritaakakom_tama;
+public class DataBerita {
+    public String judulBerita;
+    public String isiBerita;
+    public String tglPosting;
+    public String gambarBerita;
+}
